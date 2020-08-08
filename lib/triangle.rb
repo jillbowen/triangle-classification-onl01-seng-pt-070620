79799f67bc71f4836dfe942bd2325896c8ae2fab
Jill Short <jillian.b.short@gmail.com>
2020-08-08 00:51:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,8 @@
 require 'pry'
 class Triangle
-  attr_accessor :equilateral, :isoseles, :scalene
   
   def initialize(a, b, c)
-    @equilateral = equilateral
-    @isoseles = isoseles
-    @scalene = scalene
+   
   end
   
   def kind 
