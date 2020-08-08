f7ea4d866bb2daa4e6cea156e8c18b7f1813d69a
Jill Short <jillian.b.short@gmail.com>
2020-08-08 00:40:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,6 @@ class Triangle
   attr_accessor :equilateral, :isoseles, :scalene
   
   def initialize(attributes)
-    attributes.each {|k,v| self.send(("#{k}="))
+    attributes.each {|k,v| self.send(("#{k}="), value)}
   end
 end
