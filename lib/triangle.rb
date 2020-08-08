b5752260cf1912f1eb5d52555c953f1b98f8b8e9
Jill Short <jillian.b.short@gmail.com>
2020-08-08 01:43:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     if @a <= 0 || @b <= 0 || @c <= 0
       raise TriangleError
     elsif @a + @b < @c || @a + @c < @b || @b + @c <@a 
-    
+      raise TriangleError
     elsif @a == @b && @a == @c && @b == @c
       return :equilateral
     elsif @a == @b || @a == @c || @b == @c
